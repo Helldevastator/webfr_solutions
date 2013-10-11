511f21e9f86689c320dbb8ea76e0f7113ce2d79b
Roland Hediger <rolandh871@gmail.com>
2013-10-11 16:38:49 +0200

    ab5

---
+++ flashcard/src/main/java/ch/fhnw/edu/flashcard/web/IndexController.java
@@ -0,0 +1,23 @@
+package ch.fhnw.edu.flashcard.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ch.fhnw.edu.flashcard.domain.Questionnaire;
+
+@RequestMapping("/")
+@Controller
+public class IndexController {
+
+	private Logger logger = Logger.getLogger(this.getClass());
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model uiModel) {
+		uiModel.addAttribute("questionnaires", Questionnaire.findAllQuestionnaires());
+		this.logger.debug("index called");
+		return "index";
+	}
+}
